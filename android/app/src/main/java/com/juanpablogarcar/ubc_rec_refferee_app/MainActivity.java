9bdc82b0a579bc8477cfe7d622e6663fbce457d9
Juan Pablo Garcia <juapgarc@gmail.com>
2020-01-20 22:45:48 -0800

    First commit!

---
+++ android/app/src/main/java/com/juanpablogarcar/ubc_rec_refferee_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.juanpablogarcar.ubc_rec_refferee_app;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
